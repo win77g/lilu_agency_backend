116dce6879d9a73376e9dfeb04a04398e2239256
Serhii <win21g@mail.ru>
2024-01-16 17:44:28 +0100

    new

--- server.js
+++ server.js
@@ -30,9 +30,10 @@ console.log(process.env.MONGO_URI)
 
 mongoose.connect(process.env.MONGO_URI, {
 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    // useFindAndModify: false
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
     
     }, err => {
     if(err) throw err;
